b8450f698b98fcb60093c8df04da3b5da94ba969
Oliver Viljamaa <oliver.viljamaa@transferwise.com>
2017-01-23 22:43:25 +0000

    Add screening getter for Forum Cinemas

---
+++ src/screening-getters/getForumCinemasScreeningsFromHtml/index.js
@@ -0,0 +1,77 @@
+const Promise = require('bluebird');
+const jsdom = Promise.promisifyAll(require('jsdom'));
+const moment = require('moment');
+
+const { findAll, removeUnnecessarySpaces } = require('../utils');
+const { getLanguage, getDimensions } = require('../movieFormatUtils');
+
+const getMovieNodesFromDocument = document => findAll(document, '.result');
+
+const getFormatNode = node => node.querySelector('.result_h4').parentNode;
+
+const getLanguageFromNode =
+  node => node.childNodes.length > 2 // eslint-disable-line no-confusing-arrow
+    ? getLanguage(removeUnnecessarySpaces(node.childNodes[2].textContent))
+    : null;
+
+const getDimensionsFromNode =
+  node => node.childNodes.length > 2 // eslint-disable-line no-confusing-arrow
+    ? getDimensions(removeUnnecessarySpaces(node.childNodes[2].textContent))
+    : null;
+
+const getTitleNode = node => node.querySelector('.result_h4').parentNode.nextElementSibling;
+
+const getMovieScreeningNodes = node => findAll(node, '.showTime a');
+
+const getSelectedDate = node => node.querySelector('select[name=dt] option[selected=selected]').value;
+
+const getTimeForDocumentFromNode = (document, node) => moment(
+  `${getSelectedDate(document)} ${removeUnnecessarySpaces(node.textContent)}`,
+  'DD.MM.YYYY HH:mm',
+).toDate();
+
+const getLinkFromNode = node => node.href;
+
+const getScreeningsFromDocument = document =>
+  getMovieNodesFromDocument(document).reduce((screenings, movieNode) => {
+    const estonianTitle = removeUnnecessarySpaces(
+      movieNode.querySelector('.result_h4').textContent,
+    );
+
+    const formatNode = getFormatNode(movieNode);
+    const language = getLanguageFromNode(formatNode);
+    const dimensions = getDimensionsFromNode(formatNode);
+
+    const titleNode = getTitleNode(movieNode);
+    const title = removeUnnecessarySpaces(titleNode.textContent);
+
+    const year = null;
+    const isImax = false;
+
+    const movieScreenings = getMovieScreeningNodes(movieNode).map(node => ({
+      title,
+      year,
+      estonianTitle,
+      time: getTimeForDocumentFromNode(document, node),
+      link: getLinkFromNode(node),
+      language,
+      dimensions,
+      isImax,
+    }));
+
+    return [
+      ...screenings,
+      ...movieScreenings,
+    ];
+  }, []);
+
+const getApolloScreeningsFromHtml = html =>
+  jsdom
+    .envAsync(html)
+    .then(window => window.document)
+    .then(getScreeningsFromDocument)
+    .catch((err) => {
+      throw new Error(err);
+    });
+
+module.exports = getApolloScreeningsFromHtml;
---
+++ src/screening-getters/getForumCinemasScreeningsFromHtml/spec.js
@@ -0,0 +1,99 @@
+const getForumCinemasScreeningsFromHtml = require('./');
+
+const testHtml = require('./testHtml');
+
+describe('getForumCinemasScreeningsFromHtml', () => {
+  it('gets correct number of screenings', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      expect(screenings).toHaveLength(5);
+    }),
+  );
+
+  it('gets screenings with title', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const titles = screenings.map(screening => screening.title);
+
+      expect(titles).toEqual([
+        'Три богатыря и Морской царь',
+        'Три богатыря и Морской царь',
+        'Bodom',
+        'Три богатыря и Морской царь',
+        "Assassin's Creed",
+      ]);
+    }),
+  );
+
+  it('gets screenings with year', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const years = screenings.map(screening => screening.year);
+
+      expect(years).toEqual([null, null, null, null, null]);
+    }),
+  );
+
+  it('gets screenings with Estonian title', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const estonianTitles = screenings.map(screening => screening.estonianTitle);
+
+      expect(estonianTitles).toEqual([
+        'Kolm vägilast ja merekuningas',
+        'Kolm vägilast ja merekuningas',
+        'Bodom',
+        'Kolm vägilast ja merekuningas',
+        "Assassin's Creed",
+      ]);
+    }),
+  );
+
+  it('gets screenings with time', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const times = screenings.map(screening => screening.time);
+
+      expect(times).toEqual([
+        new Date(2017, 0, 23, 11, 15),
+        new Date(2017, 0, 23, 17),
+        new Date(2017, 0, 23, 11, 35),
+        new Date(2017, 0, 23, 13, 35),
+        new Date(2017, 0, 23, 14, 45),
+      ]);
+    }),
+  );
+
+  it('gets screenings with link', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const links = screenings.map(screening => screening.link);
+
+      expect(links).toEqual([
+        'https://www.forumcinemas.ee/Websales/Show/284525/',
+        'https://www.forumcinemas.ee/Websales/Show/284526/',
+        'https://www.forumcinemas.ee/Websales/Show/284478/',
+        'https://www.forumcinemas.ee/Websales/Show/284522/',
+        'https://www.forumcinemas.ee/Websales/Show/284514/',
+      ]);
+    }),
+  );
+
+  it('gets screenings with language', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const languages = screenings.map(screening => screening.language);
+
+      expect(languages).toEqual(['ru', 'ru', null, 'et', null]);
+    }),
+  );
+
+  it('gets screenings with dimensions', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const dimensions = screenings.map(screening => screening.dimensions);
+
+      expect(dimensions).toEqual([null, null, null, null, 3]);
+    }),
+  );
+
+  it('gets screenings with imax', () =>
+    getForumCinemasScreeningsFromHtml(testHtml).then((screenings) => {
+      const imaxes = screenings.map(screening => screening.isImax);
+
+      expect(imaxes).toEqual([false, false, false, false, false]);
+    }),
+  );
+});
