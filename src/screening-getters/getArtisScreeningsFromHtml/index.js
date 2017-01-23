eb4dfa3e70bc98e92b766f8786f49d2b3aaffd8f
Oliver Viljamaa <oliver.viljamaa@transferwise.com>
2017-01-23 23:55:44 +0000

    Add screening getter for Artis

---
+++ src/screening-getters/getArtisScreeningsFromHtml/index.js
@@ -0,0 +1,74 @@
+const Promise = require('bluebird');
+const jsdom = Promise.promisifyAll(require('jsdom'));
+const moment = require('moment');
+
+const { findAll, removeUnnecessarySpaces } = require('../utils');
+const { getLanguage } = require('../movieFormatUtils');
+
+
+const getMovieNodesFromDocument = document => findAll(document, '.eventListItem');
+
+const getEstonianTitleNode = node => node.querySelector('.title-big');
+
+const getEstonianTitle = node => removeUnnecessarySpaces(getEstonianTitleNode(node).textContent);
+
+const getTitleNode = node => node.querySelector('.title-original');
+
+const getTitle = node => removeUnnecessarySpaces(getTitleNode(node).childNodes[2].textContent);
+
+const hasSubtitles = node => !!node.querySelector('.subs');
+
+const getAudioLanguageNode = node => node.querySelector('.language');
+
+const getLanguageFromNode = node => hasSubtitles(node) // eslint-disable-line no-confusing-arrow
+  ? null
+  : getLanguage(getAudioLanguageNode(node).textContent);
+
+const getMovieScreeningNodes = node => findAll(node, '.ajad a');
+
+const getSelectedDate = node => node.querySelector('select[name=dt] option[selected=selected]').value;
+
+const getTimeForDocumentFromNode = (document, node) => moment(
+  `${getSelectedDate(document)} ${removeUnnecessarySpaces(node.textContent)}`,
+  'DD.MM.YYYY HH:mm',
+).toDate();
+
+const getLinkFromNode = node => `http://www.kino.ee${node.href}`;
+
+const getScreeningsFromDocument = document =>
+  getMovieNodesFromDocument(document).reduce((screenings, movieNode) => {
+    const title = getTitle(movieNode);
+    const estonianTitle = getEstonianTitle(movieNode);
+    const language = getLanguageFromNode(movieNode);
+
+    const year = null;
+    const dimensions = null;
+    const isImax = false;
+
+    const movieScreenings = getMovieScreeningNodes(movieNode).map(node => ({
+      title,
+      year,
+      estonianTitle,
+      time: getTimeForDocumentFromNode(document, node),
+      link: getLinkFromNode(node),
+      language,
+      dimensions,
+      isImax,
+    }));
+
+    return [
+      ...screenings,
+      ...movieScreenings,
+    ];
+  }, []);
+
+const getArtisScreeningsFromHtml = html =>
+  jsdom
+    .envAsync(html)
+    .then(window => window.document)
+    .then(getScreeningsFromDocument)
+    .catch((err) => {
+      throw new Error(err);
+    });
+
+module.exports = getArtisScreeningsFromHtml;
---
+++ src/screening-getters/getArtisScreeningsFromHtml/spec.js
@@ -0,0 +1,99 @@
+const getArtisScreeningsFromHtml = require('./');
+
+const testHtml = require('./testHtml');
+
+describe('getArtisScreeningsFromHtml', () => {
+  it('gets correct number of screenings', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      expect(screenings).toHaveLength(5);
+    }),
+  );
+
+  it('gets screenings with title', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const titles = screenings.map(screening => screening.title);
+
+      expect(titles).toEqual([
+        'Järven tarina (EST)',
+        'Elle',
+        'Elle',
+        'Kuudes kerta',
+        'Fallen City',
+      ]);
+    }),
+  );
+
+  it('gets screenings with year', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const years = screenings.map(screening => screening.year);
+
+      expect(years).toEqual([null, null, null, null, null]);
+    }),
+  );
+
+  it('gets screenings with Estonian title', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const estonianTitles = screenings.map(screening => screening.estonianTitle);
+
+      expect(estonianTitles).toEqual([
+        'Ühe järve lugu (EST)',
+        'Elle',
+        'Elle',
+        'Kuues kord',
+        'Kaotatud linn',
+      ]);
+    }),
+  );
+
+  it('gets screenings with time', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const times = screenings.map(screening => screening.time);
+
+      expect(times).toEqual([
+        new Date(2017, 0, 24, 11, 30),
+        new Date(2017, 0, 24, 12),
+        new Date(2017, 0, 24, 17, 15),
+        new Date(2017, 0, 24, 13, 5),
+        new Date(2017, 0, 24, 19, 30),
+      ]);
+    }),
+  );
+
+  it('gets screenings with link', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const links = screenings.map(screening => screening.link);
+
+      expect(links).toEqual([
+        'http://www.kino.ee/Websales/Show/113959/?dt=24.01.2017',
+        'http://www.kino.ee/Websales/Show/114008/?dt=24.01.2017',
+        'http://www.kino.ee/Websales/Show/114005/?dt=24.01.2017',
+        'http://www.kino.ee/Websales/Show/114011/?dt=24.01.2017',
+        'http://www.kino.ee/Websales/Show/113893/?dt=24.01.2017',
+      ]);
+    }),
+  );
+
+  it('gets screenings with language', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const languages = screenings.map(screening => screening.language);
+
+      expect(languages).toEqual(['et', null, null, null, null]);
+    }),
+  );
+
+  it('gets screenings with dimensions', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const dimensions = screenings.map(screening => screening.dimensions);
+
+      expect(dimensions).toEqual([null, null, null, null, null]);
+    }),
+  );
+
+  it('gets screenings with imax', () =>
+    getArtisScreeningsFromHtml(testHtml).then((screenings) => {
+      const imaxes = screenings.map(screening => screening.isImax);
+
+      expect(imaxes).toEqual([false, false, false, false, false]);
+    }),
+  );
+});
