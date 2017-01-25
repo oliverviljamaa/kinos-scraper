058c6c3111b0e03ce2a5017543eecf74e99bc85d
Oliver Viljamaa <oliver.viljamaa@transferwise.com>
2017-01-25 20:59:48 +0000

    Add screening getter for Kosmos

---
+++ src/screening-getters/getKosmosScreeningsFromHtmlForDate/index.js
@@ -0,0 +1,78 @@
+const Promise = require('bluebird');
+const jsdom = Promise.promisifyAll(require('jsdom'));
+const moment = require('moment');
+
+const { findAll, removeUnnecessarySpaces } = require('../utils');
+const { getLanguage, getDimensions, getIfIsImax } = require('../movieFormatUtils');
+
+
+const createDateString = date => moment(date).format('YYYY-MM-DD');
+
+const extractHtmlFromDocumentForDate = (document, date) =>
+  document.querySelector(`#acc${createDateString(date)}`);
+
+const getMovieNodesFromHtml = html => findAll(html, '.film-now-showing');
+
+const getEstonianTitleWithFormat = node => node.querySelector('h3 a').textContent;
+
+const cleanTitleFromFormatAndSpaces = title => removeUnnecessarySpaces(
+  title.replace(' 3D', '').replace(' IMAX', ''),
+);
+
+const getTitleWithFormat = node => node.querySelector('.originaltitle').textContent;
+
+const getMovieScreeningNodes = node => findAll(node, '.session-item');
+
+const getSelectedDate = html => html.id.substring(3);
+
+const getTimeForHtmlFromNode = (html, node) => moment(
+  `${getSelectedDate(html)} ${removeUnnecessarySpaces(node.querySelector('.time-wrap').textContent)}`,
+  'YYYY-MM-DD HH:mm',
+).toDate();
+
+const getLinkFromNode = node => node.querySelector('.time-wrap').href;
+
+const getLanguageFromNode = node => getLanguage(
+  node.querySelector('.attributes').textContent,
+);
+
+const getScreeningsFromHtml = html =>
+  getMovieNodesFromHtml(html).reduce((screenings, movieNode) => {
+    const estonianTitleWithFormat = getEstonianTitleWithFormat(movieNode);
+    const estonianTitle = cleanTitleFromFormatAndSpaces(estonianTitleWithFormat);
+    const dimensions = getDimensions(estonianTitleWithFormat);
+    const isImax = getIfIsImax(estonianTitleWithFormat);
+
+    const titleWithFormat = getTitleWithFormat(movieNode);
+    const title = cleanTitleFromFormatAndSpaces(titleWithFormat);
+
+    const year = null;
+
+    const movieScreenings = getMovieScreeningNodes(movieNode).map(node => ({
+      title,
+      year,
+      estonianTitle,
+      time: getTimeForHtmlFromNode(html, node),
+      link: getLinkFromNode(node),
+      language: getLanguageFromNode(node),
+      dimensions,
+      isImax,
+    }));
+
+    return [
+      ...screenings,
+      ...movieScreenings,
+    ];
+  }, []);
+
+const getKosmosScreeningsFromHtmlForDate = (html, date) =>
+  jsdom
+    .envAsync(html)
+    .then(window => window.document)
+    .then(document => extractHtmlFromDocumentForDate(document, date))
+    .then(getScreeningsFromHtml)
+    .catch((err) => {
+      throw new Error(err);
+    });
+
+module.exports = getKosmosScreeningsFromHtmlForDate;
---
+++ src/screening-getters/getKosmosScreeningsFromHtmlForDate/spec.js
@@ -0,0 +1,109 @@
+const getKosmosScreeningsFromHtmlForDate = require('./');
+
+const testHtml = require('./testHtml');
+
+const date = new Date(2017, 0, 26);
+
+describe('getKosmosScreeningsFromHtmlForDate', () => {
+  it('gets correct number of screenings', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      expect(screenings).toHaveLength(7);
+    }),
+  );
+
+  it('gets screenings with title', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const titles = screenings.map(screening => screening.title);
+
+      expect(titles).toEqual([
+        'Ballerina',
+        'Ballerina',
+        'xXx: The Return of Xander Cage',
+        'xXx: The Return of Xander Cage',
+        'xXx: The Return of Xander Cage',
+        'Викинг',
+        'Викинг',
+      ]);
+    }),
+  );
+
+  it('gets screenings with year', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const years = screenings.map(screening => screening.year);
+
+      expect(years).toEqual([null, null, null, null, null, null, null]);
+    }),
+  );
+
+  it('gets screenings with Estonian title', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const estonianTitles = screenings.map(screening => screening.estonianTitle);
+
+      expect(estonianTitles).toEqual([
+        'Baleriin',
+        'Baleriin',
+        'xXx: Käima tõmmatud',
+        'xXx: Käima tõmmatud',
+        'xXx: Käima tõmmatud',
+        'Viiking',
+        'Viiking',
+      ]);
+    }),
+  );
+
+  it('gets screenings with time', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const times = screenings.map(screening => screening.time);
+
+      expect(times).toEqual([
+        new Date(2017, 0, 26, 13, 10),
+        new Date(2017, 0, 26, 15, 15),
+        new Date(2017, 0, 26, 13, 30),
+        new Date(2017, 0, 26, 16),
+        new Date(2017, 0, 26, 21, 45),
+        new Date(2017, 0, 26, 16),
+        new Date(2017, 0, 26, 19, 45),
+      ]);
+    }),
+  );
+
+  it('gets screenings with link', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const links = screenings.map(screening => screening.link);
+
+      expect(links).toEqual([
+        'https://kinokosmos.ee/piletid/?sessionId=13548',
+        'https://kinokosmos.ee/piletid/?sessionId=13549',
+        'https://kinokosmos.ee/piletid/?sessionId=13557',
+        'https://kinokosmos.ee/piletid/?sessionId=13558',
+        'https://kinokosmos.ee/piletid/?sessionId=13559',
+        'https://kinokosmos.ee/piletid/?sessionId=13554',
+        'https://kinokosmos.ee/piletid/?sessionId=13551',
+      ]);
+    }),
+  );
+
+  it('gets screenings with language', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const languages = screenings.map(screening => screening.language);
+
+      expect(languages).toEqual(['et', 'et', null, null, null, 'ru', 'ru']);
+    }),
+  );
+
+  it('gets screenings with dimensions', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const dimensions = screenings.map(screening => screening.dimensions);
+
+      expect(dimensions).toEqual([null, null, 3, 3, 3, null, null]);
+    }),
+  );
+
+  it('gets screenings with imax', () =>
+    getKosmosScreeningsFromHtmlForDate(testHtml, date).then((screenings) => {
+      const imaxes = screenings.map(screening => screening.isImax);
+
+      expect(imaxes).toEqual([false, false, true, true, true, false, false]);
+    }),
+  );
+});
